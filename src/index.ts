4b243c43657d825a5587c9285a4cb374e7ac24d2
Alex Vong <avong2011@gmail.com>
2021-04-29 07:23:37 -0400

    feat: 05 Transform Objects

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import './scss/app.scss';
 import * as THREE from 'three';
 
-// Scene
+// Canvas
 const canvas: HTMLCanvasElement = document.querySelector('canvas.webgl');
+// Scene
 const scene = new THREE.Scene();
 
 // Object
@@ -10,6 +11,13 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: 0xff0000});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Positions
+mesh.position.set(0.7, -0.6, 1);
+mesh.scale.set(2, 0.5, 0.5);
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 // Sizes
 const sizes = {
@@ -20,11 +28,13 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+// camera.position.y = 1;
+// camera.position.x = 1;
 scene.add(camera);
 
+console.log(mesh.position.distanceTo(camera.position));
+
 // Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-});
+const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
