1359e263ee5fbe273d63b7971db56c6486689758
Alex Vong <avong2011@gmail.com>
2021-05-05 07:50:44 -0400

    feat: added ui controls

--- src/index.ts
+++ src/index.ts
@@ -27,41 +27,53 @@ window.addEventListener('resize', () => {
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
+// UI Debugger
+const gui = new dat.GUI();
+const colors = {
+  specular: '#0f72fc',
+};
 // Init a THREE scene
 const scene = new THREE.Scene();
 
 // Load texture
 const textureLoader = new THREE.TextureLoader();
-const golden = textureLoader.load(matCapAsset);
 const baseTexture = textureLoader.load(base);
 const normalTexture = textureLoader.load(normal);
 const roughnessTexture = textureLoader.load(roughness);
 const dispTexture = textureLoader.load(disp);
 const ambientTexture = textureLoader.load(ambient);
 
-// Add geometries
-const geometry = new THREE.SphereGeometry(1, 100, 100);
-const material = new THREE.MeshPhongMaterial({
-  color: 'lightblue',
-  wireframe: false,
-  map: baseTexture,
-  aoMap: ambientTexture,
-  normalMap: normalTexture,
-  shininess: 64,
-  reflectivity: 0.2,
-  specular: 'lightblue',
-  bumpMap: roughnessTexture,
-  bumpScale: 0.1,
-  displacementMap: dispTexture,
-  displacementScale: 0.12,
-  displacementBias: 0.2,
-});
-const mesh = new THREE.Mesh(geometry, material);
-mesh.receiveShadow = true;
-mesh.geometry.setAttribute('uv2', new THREE.BufferAttribute(mesh.geometry.attributes.uv.array, 2));
-
-scene.add(mesh);
+// Add geometries & material
+const gem = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 100, 100),
+  new THREE.MeshPhongMaterial({
+    wireframe: false,
+    map: baseTexture,
+    aoMap: ambientTexture,
+    normalMap: normalTexture,
+    shininess: 64,
+    reflectivity: 0.2,
+    specular: colors.specular,
+    bumpMap: roughnessTexture,
+    bumpScale: 0.1,
+    displacementMap: dispTexture,
+    displacementScale: 0.12,
+    displacementBias: 0.2,
+  })
+);
+gem.geometry.setAttribute('uv2', new THREE.BufferAttribute(gem.geometry.attributes.uv.array, 2));
+gui.add(gem.material, 'wireframe').name('Wireframe');
+gui.add(gem.material, 'shininess', 0, 100, 1).name('Shininess');
+gui.add(gem.material, 'reflectivity', 0, 1, 0.01).name('Reflections');
+gui.add(gem.material, 'bumpScale', 0, 1, 0.01).name('Bumps');
+gui.add(gem.material, 'displacementScale', 0, 1, 0.01).name('Displacements');
+gui.add(gem.material, 'displacementBias', 0, 1, 0.01).name('Displacements bias');
+gui
+  .addColor(colors, 'specular')
+  .name('Specular hue')
+  .onChange(() => {
+    gem.material.specular.set(colors.specular);
+  });
 
 // Lights
 const ambientLight = new THREE.AmbientLight('white', 0.5);
@@ -69,12 +81,9 @@ const pointLight = new THREE.PointLight('white', 0.7);
 pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
-scene.add(ambientLight, pointLight);
-
-const axesHelper = new THREE.AxesHelper(2);
-// scene.add(axesHelper);
 
 // Add to scene
+scene.add(ambientLight, pointLight, gem);
 // Add camera and define it's Z axis and FOV
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 100);
 camera.position.z = 6;
@@ -85,16 +94,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.render(scene, camera);
 //const clock = new THREE.Clock()
 
-// gsap.to(mesh.rotation, {
-//   duration: 3,
-//   ease: 'linear',
-//   y: Math.PI * 2,
-//   repeat: -1,
-// });
-
 gsap.ticker.add((time) => {
-  mesh.rotation.x = Math.cos(time / 2);
-  mesh.rotation.y = Math.sin(time / 2);
+  gem.rotation.x = Math.cos(time / 2);
+  gem.rotation.y = Math.sin(time / 2);
   // camera.lookAt(mesh.position);
   renderer.render(scene, camera);
 });
