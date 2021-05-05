76f67fef85b20ba2288429072d13104bac520456
Alex Vong <avong2011@gmail.com>
2021-05-05 07:23:34 -0400

    feat: playing with textures and lighting

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import * as THREE from 'three';
 import {gsap} from 'gsap';
 import * as dat from 'dat.gui';
 import matCapAsset from './assets/matcaps/8.png';
-import base from './assets/flower/Flower_Bud_001_basecolor.jpg';
-import normal from './assets/flower/Flower_Bud_001_normal.jpg';
-import roughness from './assets/flower/Flower_Bud_001_roughness.jpg';
-import height from './assets/flower/Flower_Bud_001_height.png';
-import ambient from './assets/flower/Flower_Bud_001_ambientOcclusion.jpg';
+import base from './assets/gem/color.jpg';
+import normal from './assets/gem/normal.jpg';
+import roughness from './assets/gem/rough.jpg';
+import disp from './assets/gem/disp.png';
+import ambient from './assets/gem/ambient.jpg';
 
 // Create a canvas
 const canvas: HTMLCanvasElement = document.querySelector('canvas.webgl');
@@ -37,32 +37,39 @@ const golden = textureLoader.load(matCapAsset);
 const baseTexture = textureLoader.load(base);
 const normalTexture = textureLoader.load(normal);
 const roughnessTexture = textureLoader.load(roughness);
-const heightTexture = textureLoader.load(height);
+const dispTexture = textureLoader.load(disp);
 const ambientTexture = textureLoader.load(ambient);
 
 // Add geometries
 const geometry = new THREE.SphereGeometry(1, 100, 100);
-const material = new THREE.MeshStandardMaterial({
+const material = new THREE.MeshPhongMaterial({
+  color: 'lightblue',
+  wireframe: false,
   map: baseTexture,
   aoMap: ambientTexture,
   normalMap: normalTexture,
-  displacementMap: heightTexture,
-  displacementScale: 0.125,
+  shininess: 64,
+  reflectivity: 0.2,
+  specular: 'lightblue',
   bumpMap: roughnessTexture,
+  bumpScale: 0.1,
+  displacementMap: dispTexture,
+  displacementScale: 0.12,
+  displacementBias: 0.2,
 });
 const mesh = new THREE.Mesh(geometry, material);
-
+mesh.receiveShadow = true;
 mesh.geometry.setAttribute('uv2', new THREE.BufferAttribute(mesh.geometry.attributes.uv.array, 2));
 
 scene.add(mesh);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-const pointLight = new THREE.PointLight(0xffffff, 0.5);
+// Lights
+const ambientLight = new THREE.AmbientLight('white', 0.5);
+const pointLight = new THREE.PointLight('white', 0.7);
 pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
-scene.add(pointLight);
+scene.add(ambientLight, pointLight);
 
 const axesHelper = new THREE.AxesHelper(2);
 // scene.add(axesHelper);
@@ -86,8 +93,8 @@ renderer.render(scene, camera);
 // });
 
 gsap.ticker.add((time) => {
-  mesh.rotation.y = Math.sin(time * 1.4);
-  mesh.rotation.x = Math.cos(time * 1.4);
+  mesh.rotation.x = Math.cos(time / 2);
+  mesh.rotation.y = Math.sin(time / 2);
   // camera.lookAt(mesh.position);
   renderer.render(scene, camera);
 });
