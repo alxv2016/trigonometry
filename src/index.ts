35d170619b52f090a5de66d42766f6c367eeee94
Alex Vong <avong2011@gmail.com>
2021-05-14 07:18:40 -0400

    feat: polar coordinates and circular motion

--- src/index.ts
+++ src/index.ts
@@ -24,21 +24,36 @@ window.addEventListener('resize', () => {
 const scene = new THREE.Scene();
 
 // Add geometries & material
-const geometry = new THREE.BoxGeometry(4, 4, 4);
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const geometry2 = new THREE.SphereGeometry(1, 64, 64);
 const material = new THREE.MeshNormalMaterial();
-const box = new THREE.Mesh(geometry, material);
+// const box = new THREE.Mesh(geometry, material);
+const sphere = new THREE.Mesh(geometry, material);
+const sphere2 = new THREE.Mesh(geometry2, material);
 // const group = new THREE.Group();
 // const center = new THREE.Vector3();
 // new THREE.Box3().setFromObject(group).getCenter(center);
 // group.position.copy(center).multiplyScalar(-1);
+// let angle = 0;
+// let angleV = -0.06;
+// let angleA = 0.06;
+let radius = 3;
+let offset = 1.5;
+// let angle = Math.PI / 4;
 let angle = 0;
-let angleV = -0.06;
-let angleA = 0.06;
+
+function angleRotation() {
+  sphere2.position.x = radius * Math.cos(angle) * offset;
+  sphere2.position.y = radius * Math.sin(angle) * offset;
+  sphere2.position.z = radius * Math.sin(angle) * offset;
+  radius = Math.cos(angle * Math.PI * 2) * 6;
+  angle -= 0.005;
+}
 
 // Add to scene
 const axesHelper = new THREE.AxesHelper(5);
 axesHelper.visible = true;
-scene.add(axesHelper, box);
+scene.add(axesHelper, sphere, sphere2);
 // Add camera and define it's Z axis and FOV
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 800);
 camera.position.z = 90;
@@ -59,9 +74,11 @@ renderer.setClearColor('#01062D');
 
 // Animation ticks
 const tick = () => {
-  box.rotation.y = angle;
-  angle += angleV / 50;
-  angleV += angleA / 50;
+  // Rotation
+  // box.rotation.y = angle;
+  // angle += angleV / 50;
+  // angleV += angleA / 50;
+  angleRotation();
   renderer.render(scene, camera);
   controls.update();
   window.requestAnimationFrame(tick);
