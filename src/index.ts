b7254d589884f0e4e6aecfe47fbb6bc75fd4a01d
Alex Vong <avong2011@gmail.com>
2021-05-15 21:44:27 -0400

    feat: figuring out magic number

--- src/index.ts
+++ src/index.ts
@@ -24,37 +24,64 @@ window.addEventListener('resize', () => {
 const scene = new THREE.Scene();
 
 // Add geometries & material
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-// const geometry2 = new THREE.SphereGeometry(1, 64, 64);
+const geometry = new THREE.CylinderGeometry(1, 1, 2, 64);
 const material = new THREE.MeshNormalMaterial();
-// const box = new THREE.Mesh(geometry, material);
-const sphere = new THREE.Mesh(geometry, material);
-// const sphere2 = new THREE.Mesh(geometry2, material);
-// const group = new THREE.Group();
-// const center = new THREE.Vector3();
-// new THREE.Box3().setFromObject(group).getCenter(center);
-// group.position.copy(center).multiplyScalar(-1);
+const cylinder = new THREE.Mesh(geometry, material);
+const group = new THREE.Group();
 let radius = 3;
+let radius2 = 3;
 let angle = 0;
-let baseRadius = 2;
-let offset = 1;
-let velocity = 0.01;
+let baseRadius = 6;
+let offset = 4;
+let velocity = 0;
 let amplitude = 0;
 let period = 0;
+let total = 20;
 
-function sineWaveScale() {
-  radius = baseRadius + Math.sin(angle) * offset;
-  sphere.scale.x = radius;
-  sphere.scale.y = radius;
-  sphere.scale.z = radius;
-  angle += velocity / 90;
+for (let i = 0; i < total; i++) {
+  for (let j = 0; j < total; j++) {
+    const cylinder = new THREE.Mesh(geometry, material);
+    cylinder.position.x = i * 2;
+    cylinder.position.z = j * 2;
+    group.add(cylinder);
+  }
 }
 
-function sineWaveY() {
-  radius = Math.sin(angle) * offset;
-  sphere.position.y = radius;
-  angle += velocity;
+// for (let i = 0; i < total; i++) {
+//   const cylinder = new THREE.Mesh(geometry, material);
+//   cylinder.position.x = i * 4;
+//   group.add(cylinder);
+// }
+
+console.log(group.children.length);
+
+function sineWaveScale(fps: number) {
+  // for (let i = 0; i < total; i++) {
+  //     let speed = fps * 8;
+  //     let y = baseRadius + Math.sin(speed + i * 2) * offset;
+  //     group.children[i].scale.y = y;
+  // }
+  for (let i = 0; i < total; i++) {
+    for (let j = 0; j < total; j++) {
+      let x = Math.sin(i / (total / 2));
+      let y = Math.sin(j / (total / 2));
+      let disp = (x + y) * (x + y);
+      let z = baseRadius + Math.sin(fps * 2 + disp) * offset;
+      group.children[i + j * total].scale.y = z;
+      group.children[i + j * total].position.y = z;
+    }
+  }
 }
+
+const center = new THREE.Vector3();
+new THREE.Box3().setFromObject(group).getCenter(center);
+group.position.copy(center).multiplyScalar(-1);
+
+// function sineWaveY() {
+//   radius = Math.sin(angle) * offset;
+//   sphere.position.y = radius;
+//   angle += velocity;
+// }
 
 // function angleRotation() {
 //   sphere2.position.x = radius * Math.cos(angle) * offset;
@@ -67,7 +94,7 @@ function sineWaveY() {
 // Add to scene
 const axesHelper = new THREE.AxesHelper(5);
 axesHelper.visible = true;
-scene.add(axesHelper, sphere);
+scene.add(axesHelper, group);
 // Add camera and define it's Z axis and FOV
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 800);
 camera.position.z = 90;
@@ -87,9 +114,10 @@ renderer.render(scene, camera);
 renderer.setClearColor('#01062D');
 
 // Animation ticks
+const clock = new THREE.Clock();
 const tick = () => {
-  sineWaveScale();
-  sineWaveY();
+  const fps = clock.getElapsedTime();
+  sineWaveScale(fps);
   renderer.render(scene, camera);
   controls.update();
   window.requestAnimationFrame(tick);
