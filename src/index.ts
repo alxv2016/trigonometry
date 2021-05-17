17a7e6efb6a868fe5fcbc7300d256b3f3f4fa18a
Alex Vong <avong2011@gmail.com>
2021-05-17 07:34:50 -0400

    feat: breathing calculations

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,8 @@ const group = new THREE.Group();
 let radius = 3;
 let radius2 = 3;
 let angle = 0;
+let step = 0.5;
+let limit = 8;
 let baseRadius = 6;
 let offset = 4;
 let velocity = 0;
@@ -49,7 +51,7 @@ for (let i = 0; i < total; i++) {
 
 // for (let i = 0; i < total; i++) {
 //   const cylinder = new THREE.Mesh(geometry, material);
-//   cylinder.position.x = i * 4;
+//   cylinder.position.x = i * 2;
 //   group.add(cylinder);
 // }
 
@@ -57,18 +59,20 @@ console.log(group.children.length);
 
 function sineWaveScale(fps: number) {
   // for (let i = 0; i < total; i++) {
-  //     let speed = fps * 8;
-  //     let y = baseRadius + Math.sin(speed + i * 2) * offset;
+  //     let speed = fps * 6;
+  //     let x = Math.cos(i / total * (Math.PI * 2)) / 2;
+  //     let theta = -x * 6;
+  //     let y = baseRadius + Math.sin(speed + theta) * offset;
   //     group.children[i].scale.y = y;
   // }
   for (let i = 0; i < total; i++) {
     for (let j = 0; j < total; j++) {
-      let x = Math.sin(i / (total / 2));
-      let y = Math.sin(j / (total / 2));
-      let disp = (x + y) * (x + y);
-      let z = baseRadius + Math.sin(fps * 2 + disp) * offset;
+      let x = Math.cos((i / total) * (Math.PI * 2)) / 2;
+      let y = Math.cos((j / total) * (Math.PI * 2)) / 2;
+      let theta = (x + y) * (x + y) * 2;
+      let z = baseRadius + Math.sin(fps * 4 + theta) * offset;
       group.children[i + j * total].scale.y = z;
-      group.children[i + j * total].position.y = z;
+      //group.children[i + j * total].position.y = z;
     }
   }
 }
@@ -77,26 +81,14 @@ const center = new THREE.Vector3();
 new THREE.Box3().setFromObject(group).getCenter(center);
 group.position.copy(center).multiplyScalar(-1);
 
-// function sineWaveY() {
-//   radius = Math.sin(angle) * offset;
-//   sphere.position.y = radius;
-//   angle += velocity;
-// }
-
-// function angleRotation() {
-//   sphere2.position.x = radius * Math.cos(angle) * offset;
-//   sphere2.position.y = radius * Math.sin(angle) * offset;
-//   sphere2.position.z = radius * Math.sin(angle) * offset;
-//   radius = Math.cos(angle * Math.PI * 2) * 6;
-//   angle -= 0.005;
-// }
-
 // Add to scene
 const axesHelper = new THREE.AxesHelper(5);
 axesHelper.visible = true;
 scene.add(axesHelper, group);
 // Add camera and define it's Z axis and FOV
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 800);
+camera.position.x = 90;
+camera.position.y = 90;
 camera.position.z = 90;
 
 // controls
